a37888545ef2aa4e7473da85d778efb56c6e4f5a
vladislavbakaev <bakaev.98@bk.ru>
2022-01-22 22:48:01 +0300

    add mavlink points model

--- drone_missions_logs/models.py
+++ drone_missions_logs/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
+PROTOCOL_TYPE_CHOICES = [
+    ('ML', 'MavLink'),
+    ('PX4', 'PX4'),
+    ('YD', 'VorobyovYD')
+]
+
 class LogFile(models.Model):
     upload = models.FileField(upload_to="logs/")
     flight_data = models.DateTimeField()
     description = models.TextField()
     mission = models.ForeignKey('Mission', on_delete=models.CASCADE, null=True)
+    protocol_type = models.CharField(max_length=3, choices=PROTOCOL_TYPE_CHOICES, default='')
     at_create = models.DateTimeField()
 
     def delete(self, using=None, keep_parents=False):
@@ -12,7 +19,7 @@ class LogFile(models.Model):
         super().delete()
 
     class Meta:
-        db_table = 'yd_drone_logs_file'
+        db_table = 'drone_logs_file'
 
 
 class Mission(models.Model):
@@ -20,6 +27,7 @@ class Mission(models.Model):
     user_info = models.CharField(max_length=120)
     description = models.TextField()
     flight_mission_file = models.FileField(upload_to="flight_missions/")
+    protocol_type = models.CharField(max_length=3, choices=PROTOCOL_TYPE_CHOICES, default='')
     at_create = models.DateTimeField()
 
     def delete(self, using=None, keep_parents=False):
@@ -54,3 +62,24 @@ class YDMissionPoint(models.Model):
 
     class Meta:
         db_table = 'yd_drone_mission_point'
+
+class MavLinkMissionPoint(models.Model):
+    mission = models.ForeignKey(Mission, on_delete=models.CASCADE)
+    param1 = models.DecimalField(max_digits=15, decimal_places=9)
+    param2 = models.DecimalField(max_digits=15, decimal_places=9)
+    param3 = models.DecimalField(max_digits=15, decimal_places=9)
+    param4 = models.DecimalField(max_digits=15, decimal_places=9)
+    
+    x = models.DecimalField(max_digits=15, decimal_places=9)
+    y = models.DecimalField(max_digits=15, decimal_places=9)
+    z = models.DecimalField(max_digits=15, decimal_places=9)
+
+    seq = models.IntegerField()
+    command = models.IntegerField()
+    target_component = models.IntegerField()
+    frame = models.IntegerField()
+    current = models.IntegerField()
+    autocontinue = models.IntegerField()
+    
+    class Meta:
+        db_table = 'mavlink_drone_mission_point'
